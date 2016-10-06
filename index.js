b121f3aa71f6b9fa470b39be9217b805efce394e
Niels Overwijn <niels.overwijn@gmail.com>
2016-10-06 21:44:08 +0100

    Correct console log

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
   app.use(api);
 
   app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Weather Hub active on port 3000!');
   });
 
   module.exports.app = app;
