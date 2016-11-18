af2b1873e7350ebb6ca542aa587c3ff3e2224e51
Niels Overwijn <niels.overwijn@gmail.com>
2016-11-18 07:57:37 +0000

    Add CORS Header

--- index.js
+++ index.js
@@ -11,6 +11,11 @@
   const api = require('./api.js');
 
   app.use(bodyParser.json());
+  app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   });
   app.use(api);
 
   app.listen(3000, function () {
